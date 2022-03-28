451c077087e1f5883e914ab4ecdd9a1afc5f0550
Aakash <kaushikaakash527@gmail.com>
2022-03-28 21:08:29 +0530

    day 2 completed

---
+++ lib/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("first app"),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text("this is my try flutter app"),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/logic.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Loginpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Center(child: Text("Login page")),
+    );
+  }
+}
